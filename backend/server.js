ebc7e2786a8950b588dd72ea3a8c6844c6c99ff7
Paulo Cornachioni <92116855+PHCornachioni@users.noreply.github.com>
2026-04-13 19:58:20 -0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -160,7 +160,7 @@ app.put('/api/locais/:id', async (req, res) => {
 });
 
 // Iniciando Servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
